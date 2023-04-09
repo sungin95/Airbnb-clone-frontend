eb558462c6516979bcfb6c08366b63957b236ce8
Sungin Lee <dlrkehrud@naver.com>
2023-04-09 11:55:52 +0900

    20.10 kakao Talk Auth, 복사(라우터, 소셜 로그인, 클릭 페이지, 코드 받기)

---
+++ src/routes/KakaoConfirm.tsx
@@ -0,0 +1,42 @@
+import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { kakaoLogIn } from "./api";
+
+export default function KakaoConfirm() {
+  // useLocation은 우리가 있는 곳을 알려준다.
+  const { search } = useLocation();
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const confirmLogin = async () => {
+    const params = new URLSearchParams(search);
+    const code = params.get("code");
+    if (code) {
+      console.log(code);
+      return;
+      const status = await kakaoLogIn(code);
+      if (status === 200) {
+        toast({
+          status: "success",
+          title: "Welcome!",
+          position: "bottom-right",
+          description: "Happy to have you back!",
+        });
+        queryClient.refetchQueries(["me"]);
+        navigate("/");
+      }
+    }
+  };
+  useEffect(() => {
+    confirmLogin();
+  }, []);
+  return (
+    <VStack justifyContent={"center"} mt={40}>
+      <Heading>Processing log in ...</Heading>
+      <Text>Don't go anywhere.</Text>
+      <Spinner size="lg" />
+    </VStack>
+  );
+}
