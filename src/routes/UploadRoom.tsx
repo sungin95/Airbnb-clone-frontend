be8ad6215e2371a7ade056490a4774ef027ac84b
Sungin Lee <dlrkehrud@naver.com>
2023-04-17 15:11:38 +0900

    21.0 add UploadRoom, ProtectedPage, HostOnlyPage

---
+++ src/routes/UploadRoom.tsx
@@ -0,0 +1,11 @@
+import useHostOnlyPage from "../components/HostOnlyPage";
+import ProtectedPage from "../components/ProtectedPage";
+
+export default function UploadRoom() {
+  useHostOnlyPage();
+  return (
+    <ProtectedPage>
+      <h1>Upload Room</h1>
+    </ProtectedPage>
+  );
+}
